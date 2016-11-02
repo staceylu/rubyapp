d07cd01a383af25d5f124df4569d8c366ae1b2b0
staceylu <contact@stacey-williams.com>
2016-11-02 07:32:06 -0500

    Added Products

--- nameofapp/app/models/product.rb
+++ nameofapp/app/models/product.rb
@@ -1,2 +1,24 @@
 class Product < ApplicationRecord
+	Product.all =[
+		{
+			name: "Product 1",
+			description: "This is the first product",
+			image_url: "dot.png"
+		}.
+		{
+			name: "Product 2",
+			description: "This is the second product",
+			image_url: "dot.png"
+		}.
+		{
+			name: "Product 3",
+			description: "This is the third product",
+			image_url: "dot.png"
+		}.
+		{
+			name: "Product 4",
+			description: "This is the fourth product",
+			image_url: "dot.png"
+		}.
+	]
 end
