2e3b9c6246d3c113117b39dd0098cc503ce1e0fc
staceylu <contact@stacey-williams.com>
2016-11-02 13:05:02 -0500

    Added products using seeds

--- nameofapp/db/seeds.rb
+++ nameofapp/db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+
+Product.create(:name => "Product3", :price => "95", :image_url => "dot.png", :description => "Look at me using seeds")
+Product.create(:name => "Product4", :price => "300", :image_url => "http://static.wixstatic.com/media/fe994a_30f79eef57c94204b3b8077797dd942b~mv2_d_2366_2366_s_2.png", :description => "I used seeds again!!")
