2f661c217c5a8987e8e38adfc8f0a25803cf6c37
staceylu <contact@stacey-williams.com>
2016-11-02 13:38:15 -0500

    fixed product order

--- nameofapp/db/seeds.rb
+++ nameofapp/db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Product.create(:name => "Product3", :price => "95", :image_url => "dot.png", :description => "Look at me using seeds")
-Product.create(:name => "Product4", :price => "300", :image_url => "http://static.wixstatic.com/media/fe994a_30f79eef57c94204b3b8077797dd942b~mv2_d_2366_2366_s_2.png", :description => "I used seeds again!!")
+Product.create(:name => "Product3", :price => "95", :image_url => "http://static.wixstatic.com/media/fe994a_30f79eef57c94204b3b8077797dd942b~mv2_d_2366_2366_s_2.png", :description => "Look at me using seeds")
+Product.create(:name => "Product4", :price => "300", :image_url => "dot.png", :description => "I used seeds again!!")
